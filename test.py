4f273c3de66ebd7a7c583030dfcb479e76cde25e
Tom Aldcroft <taldcroft@gmail.com>
2009-06-12 16:19:14 -0400

    Minor tweaks for distribution and add a couple more simple test cases

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
+import nose.tools as nt
+
 import Chandra.ECF
-import matplotlib.pyplot as plt
 import numpy as np
 
 def test_ecf_radius_plot():
-    """Qualitatively compare plot with ecf_radius_POG.png"""
+    """Qualitatively compare ecf_radius.png with ecf_radius_POG.png"""
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        return
+
     thetas = range(0, 16)
     plt.figure(1, figsize=(5,5))
     for energy in (1.49, 6.4):
@@ -17,6 +23,8 @@ def test_ecf_radius_plot():
     plt.ylim(0,30)
     plt.savefig('ecf_radius.png')
     
-                    
-    
-    
+def test_interp_ECF():
+    nt.assert_almost_equal(Chandra.ECF.interp_ECF(0.5, 5, 45, 2.0), 1.8351397514)
+
+def test_ECF_radius():
+    nt.assert_almost_equal(Chandra.ECF.ECF_radius(1.8, 5, 45, 2.0), 0.4882049750)
